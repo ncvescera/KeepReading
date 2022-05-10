bb7cc617cdf007254e8b22692c947e8207e00364
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-05-10 09:56:28 +0200

    Rinominata sottocartella assets
    Ora ha un nome sensato

--- lib/pdf_viewer.dart
+++ lib/pdf_viewer.dart
@@ -37,73 +37,73 @@ class PDFViewer extends StatelessWidget {
             child: Row(
               children: [
                 IconButton(
-                  icon: Image.asset('assets/icons/wires.png'),
+                  icon: Image.asset('assets/button_icons/wires.png'),
                   onPressed: () {
                     controller.jumpToPage(5); // normal wires
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/button.png'), // button
+                  icon: Image.asset('assets/button_icons/button.png'), // button
                   onPressed: () {
                     controller.jumpToPage(6);
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/simbol.png'),
+                  icon: Image.asset('assets/button_icons/simbol.png'),
                   onPressed: () {
                     controller.jumpToPage(7); // icons
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/saymon.png'),
+                  icon: Image.asset('assets/button_icons/saymon.png'),
                   onPressed: () {
                     controller.jumpToPage(8); // Simon says
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/text.png'),
+                  icon: Image.asset('assets/button_icons/text.png'),
                   onPressed: () {
                     controller.jumpToPage(9); // screen words
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/number.png'),
+                  icon: Image.asset('assets/button_icons/number.png'),
                   onPressed: () {
                     controller.jumpToPage(11); // screen numbers
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/morse.png'),
+                  icon: Image.asset('assets/button_icons/morse.png'),
                   onPressed: () {
                     controller.jumpToPage(12); // morse
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/complex.png'),
+                  icon: Image.asset('assets/button_icons/complex.png'),
                   onPressed: () {
                     controller.jumpToPage(13); // complex wires
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/cross.png'),
+                  icon: Image.asset('assets/button_icons/cross.png'),
                   onPressed: () {
                     controller.jumpToPage(14); // crossed wires
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/maze.png'),
+                  icon: Image.asset('assets/button_icons/maze.png'),
                   onPressed: () {
                     controller.jumpToPage(15); // maze
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/pass.png'),
+                  icon: Image.asset('assets/button_icons/pass.png'),
                   onPressed: () {
                     controller.jumpToPage(16); // password
                   },
                 ),
                 IconButton(
-                  icon: Image.asset('assets/icons/knob.png'),
+                  icon: Image.asset('assets/button_icons/knob.png'),
                   onPressed: () {
                     controller.jumpToPage(20); // knobs
                   },
